28cd27c2165c3f63c512b50f6c90663a7e402cda
2seunghye <tmdgp970120@naver.com>
2020-09-11 11:12:30 +0900

    prompt로 query값 받아오기 기능 추가

--- 10.kakaoAPI/index.js
+++ 10.kakaoAPI/index.js
@@ -1,10 +1,12 @@
 $.ajax({
   method: 'GET',
   url: 'https://dapi.kakao.com/v3/search/book?target=tditle',
-  data: { query: '정의란 무엇인가' },
+  data: { query: prompt('책 제목을 입력해주세요', '') },
   headers: { Authorization: 'KakaoAK dc9d70e5667aecff7649401efacca9f1' },
 }).done(function (msg) {
   console.log(msg);
+  $('#search').append(msg.documents[0].title.substr(0, 20));
+
   $('#title').append(msg.documents[0].title);
   $('#thumbnail').append('<img src=' + msg.documents[0].thumbnail + '>');
   $('#contents').append(msg.documents[0].contents + '...');
