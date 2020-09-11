acc50216f4e4094e0d4d7eeadf193ea38c4241f6
2seunghye <tmdgp970120@naver.com>
2020-09-11 19:46:00 +0900

    mm

--- 10.kakaoAPI/index.js
+++ 10.kakaoAPI/index.js
@@ -5,17 +5,20 @@ $.ajax({
   headers: { Authorization: 'KakaoAK dc9d70e5667aecff7649401efacca9f1' },
 }).done(function (msg) {
   console.log(msg);
-  $('#search').append(msg.documents[0].title.substr(0, 20));
+  var data = msg.documents[0];
+  $('#search').append(data.title.substr(0, 20));
 
-  $('#title').append(msg.documents[0].title);
-  $('#thumbnail').append('<img src=' + msg.documents[0].thumbnail + '>');
-  $('#contents').append(msg.documents[0].contents + '...');
+  $('#title').append(data.title);
+  $('#thumbnail').append('<img src=' + data.thumbnail + '>');
+  $('#contents').append(data.contents + '...');
 
-  $('#status').append(msg.documents[0].status);
-  $('#price').append(msg.documents[0].price);
-  $('#sale_price').append(msg.documents[0].sale_price);
-  $('#authors').append(msg.documents[0].authors);
-  $('#translators').append(msg.documents[0].translators);
-  $('#publisher').append(msg.documents[0].publisher);
-  $('#datetime').append(msg.documents[0].datetime.substr(0, 10));
+  $('#status').append(data.status);
+  $('#price').append(data.price);
+  $('#sale_price').append(data.sale_price);
+  $('#authors').append(data.authors);
+  $('#translators').append(data.translators);
+  $('#publisher').append(data.publisher);
+  $('#datetime').append(data.datetime.substr(0, 10));
 });
+
+// 검색어를 받아올 때 까지 안뜨게 ..????????
